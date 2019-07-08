c254066eb68ab7503b47c4d6a6c8c7603cf60316
wmaithil <wmaithil@gmail.com>
2019-07-08 14:39:00 +0530

    Delete search.model.js

--- search.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const NoteSchema = mongoose.Schema({
-
-"district|block|cluster|schoolid|schoolname|category|gender|medium_of_inst|address|area|pincode|landmark|identification1|busroutes|identification2|latlong": "String+String+String+String+String+String+String+String+String+String+String+String+String+String+String+String"
-
-});
-
-module.exports = mongoose.model('Note', NoteSchema);
